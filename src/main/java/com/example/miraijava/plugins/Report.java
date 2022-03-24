ac7f0b475de2ceb4bd7ce2983b8ddcf4dca1a493
戴林峰 <dailinfeng66@163.com>
2022-03-24 13:53:53 +0800

    add README

---
+++ src/main/java/com/example/miraijava/plugins/Report.java
@@ -0,0 +1,19 @@
+package com.example.miraijava.plugins;
+
+import com.example.miraijava.annotation.Plugin;
+import com.example.miraijava.interfaces.CommandHandle;
+import net.mamoe.mirai.event.events.MessageEvent;
+
+/**
+ * @Author dailinfeng
+ * @Description TODO
+ * @Date 2022/3/24 13:46
+ * @Version 1.0
+ */
+@Plugin(command = "报备")
+public class Report implements CommandHandle {
+    @Override
+    public void handleCommand(MessageEvent event) {
+
+    }
+}
