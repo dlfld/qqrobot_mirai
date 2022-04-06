5836a8157034023923762b5a2dc6e6ac24c2fa67
戴林峰 <dailinfeng66@163.com>
2022-04-06 14:49:21 +0800

    fix

--- src/main/java/com/idse/MiraiJavaApplication.java
+++ src/main/java/com/idse/MiraiJavaApplication.java
@@ -14,7 +14,11 @@ public class MiraiJavaApplication {
     public static void main(String[] args) {
 
 
-
+//        new StartBot().run(new MiraiConfig()
+//                .setQq("460796757")
+//                .setPassword("2441086385dlf..")
+//                .setPluginsDir("com.idse.miraijava.plugins") //插件包的路径，插件包需要自己创建 类似于Spring Mvc中的controller包
+//                .setWorkDir("src/main/resources/work_dir")); //work_dir的路径
         SpringApplication.run(MiraiJavaApplication.class, args);
 
     }
